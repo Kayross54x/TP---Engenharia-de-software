11442fc755582df8ac5c60b4adde0d528680e589
DESKTOP-GRBB3NJ\Kayque <kayque.original@hotmail.com>
2024-10-29 14:47:40 -0300

    implementando função de logout de usuário no header

--- tp_eng/src/components/Header.tsx
+++ tp_eng/src/components/Header.tsx
@@ -1,30 +1,43 @@
 "use client";
-import React, { useContext } from 'react';
-import Link from 'next/link';
-import { UserContext } from '@/context/UserContext';
+import React, { useContext } from "react";
+import Link from "next/link";
+import { UserContext } from "@/context/UserContext";
+import { useRouter } from "next/navigation";
 
 export default function Header() {
-	const { userLogged } = useContext(UserContext);
+	const navigate = useRouter();
+    const { userLogged, setUserLogged } = useContext(UserContext);
 
-	return (
-		<header className="flex justify-between items-center p-4 bg-gray-800 text-white">
-			<Link href="/" passHref>
-				<h1 className="text-3xl font-bold">ProcessJur</h1>
-			</Link>
+	function onLoggout() {
+		setUserLogged(null);
+		navigate.push("/");
+	}
 
-			<div>
-				{userLogged ? (
-					<Link href="/user" passHref>
-						<div>{userLogged.name}</div>
-					</Link>
-				) : (
-					<Link href="/login" passHref>
-						<button className="rounded border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-8 sm:h-9 px-4">
-							Logar
+    return (
+        <header className="block sm:flex justify-between items-center p-4 bg-gray-800 text-white">
+            <Link href="/" passHref className="flex items-center justify-center sm:justify-start sm:items-start">
+                <h1 className="text-3xl font-bold mb-4 sm:mb-0">ProcessJur</h1>
+            </Link>
+
+            <div>
+                {userLogged ? (
+                    <div className="flex items-center justify-center">
+                        <Link href="/user" passHref>
+                            <div>{userLogged?.name}</div>
+                        </Link>
+
+						<button onClick={() => onLoggout()} className="ml-4 rounded border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-8 sm:h-9 px-4">
+							Sair
 						</button>
-					</Link>
-				)}
-			</div>
-		</header>
-	);
+                    </div>
+                ) : (
+                    <Link href="/login" passHref>
+                        <button className="rounded border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-8 sm:h-9 px-4">
+                            Logar
+                        </button>
+                    </Link>
+                )}
+            </div>
+        </header>
+    );
 }
